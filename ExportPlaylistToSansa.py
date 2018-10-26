09ad3ce963290d328ed005df43bb7989a3105313
eyalitki <eyal.itkin@hotmail.com>
2018-10-26 18:01:45 +0300

    Bug fixes and a more stable version

--- ExportPlaylistToSansa.py
+++ ExportPlaylistToSansa.py
@@ -12,37 +12,37 @@ from xml.sax.saxutils import unescape
 # This program was written for personal use, and than got extended for #
 # anyone that might benefit from using it for it's own needs.          #
 # Any changes\publications of this software are on the responsibility  #
-# of the user\disributer himself.                                      #
+# of the user\distributer himself.                                     #
 #                                                                      #
 # Hope the software will be helpful,                                   #
 # Eyal Itkin.                                                          #
 ########################################################################
 
-##################################################################################################
-# README:                                                                                        #
-# =======                                                                                        #
-# This scripts was written for home use by any sansa clip user                                   #
-# that aims to sync his windows media player playlists to the                                    #
-# player he owns.                                                                                #
-#                                                                                                #
-# The scripts will do the following:                                                             #
-# 1) Find and Copy the playlists songs to the player                                             #
-# 2) Configure a .m3u playlist on the sansa player (supported by the player)                     #
-# 3) Supports .mp3 songs that includes Unicode paths (not orignaly supported by the .m3u format) #
-#                                                                                                #
-# Playlist notes:                                                                                #
-# .m3u playlists are partially supported by the sansa player, and the songs are                  #
-# shown according to thir PATH NAMES, and NOT their "title" (like shown in the song list).       #
-# The script will bypass this behavior in the following way:                                     #
-# 1) .mp3 songs will be copied using the "title", as shown in the mp3 tags                       #
-# 2) In case the "title" includes Unicode chars:                                                 #
-# 2.1) In case the "comment" tag is present, it will be used as the actuall name                 #
-# 2.2) Otherwise, the user will be prompted to enter a name (ascii only).                        #
-#      This name will be prefixed using '_' and stored in the "comment" tag for later use        #
-#                                                                                                #
-# The script is configurable, and any user that want to save it's Unicode replacement            #
-# names with another prefix, or another tag, can freely do it.                                   #
-##################################################################################################
+####################################################################################################
+# README:                                                                                          #
+# =======                                                                                          #
+# This scripts was written for home use by any sansa clip user                                     #
+# that aims to sync his windows media player playlists to the                                      #
+# player he owns.                                                                                  #
+#                                                                                                  #
+# The scripts will do the following:                                                               #
+# 1) Find and Copy the playlists songs to the player                                               #
+# 2) Configure a .m3u playlist on the sansa player (supported by the player)                       #
+# 3) Supports .mp3 songs that includes Unicode paths (not originally supported by the .m3u format) #
+#                                                                                                  #
+# Playlist notes:                                                                                  #
+# .m3u playlists are partially supported by the sansa player, and the songs are                    #
+# shown according to their PATH NAMES, and NOT their "title" (like shown in the song list).        #
+# The script will bypass this behavior in the following way:                                       #
+# 1) .mp3 songs will be copied using the "title", as shown in the mp3 tags                         #
+# 2) In case the "title" includes Unicode chars:                                                   #
+# 2.1) In case the "comment" tag is present, it will be used as the actual name                    #
+# 2.2) Otherwise, the user will be prompted to enter a name (ascii only).                          #
+#      This name will be prefixed using '_' and stored in the "comment" tag for later use          #
+#                                                                                                  #
+# The script is configurable, and any user that want to save it's Unicode replacement              #
+# names with another prefix, or another tag, can freely do it.                                     #
+####################################################################################################
 
 ###################
 # dynamic configs #
@@ -63,10 +63,10 @@ playlist_names		= [] # Configure this field.
 						 # Should include a list of playlist file names (.wpl files) from the above directory.
 my_playlists 		= map(lambda x : playlist_base_path + x, playlist_names)
 
-####################################################################################
-# NOTE: additional absolute paths can be added manualy to the "my_playlists" list. #
-# This lisw will be used by the rest of the script.                                #
-####################################################################################
+#####################################################################################
+# NOTE: additional absolute paths can be added manually to the "my_playlists" list. #
+# This lisw will be used by the rest of the script.                                 #
+#####################################################################################
 
 # unicode configurations
 unicode_prefix = '_'
@@ -81,6 +81,10 @@ song_prefix = '<media src="'
 # sansa player constants
 music_folder 	= sansa_dir + "Music\\"
 playlist_folder = sansa_dir + "Playlists\\"
+
+
+# Global variables
+seenSongs = {}
 
 ##
 # Exports the songs list to a newly created playlist
@@ -113,7 +117,7 @@ def getPlayerSongs() :
 	# list all songs in the music base path
 	for file in os.listdir(music_folder) :
 		suffix = file.split('.')[-1].lower()
-		if suffix in ['mp3', 'wmv', 'mp4'] :
+		if suffix in ['mp3', 'wmv', 'mp4', 'm4a'] :
 			songs.append(music_folder + file)
 	
 	return songs
@@ -133,12 +137,12 @@ def updateFileName(abspath, name) :
 	can_convert		= suffix.lower() == 'mp3'
 	# check the name for unicode chars
 	need_convert	= not is_ascii(name)
+	manual_help		= False
 	
 	# decide what to do
 	# need to convert and can't do it :(
 	if need_convert and not can_convert :
-		print 'Cant handle unicode named string - not an .mp3 song - %s' % repr(abspath)
-		return None
+		manual_help = True
 		
 	# can't convert, but the name is ascii
 	elif not can_convert :
@@ -154,20 +158,33 @@ def updateFileName(abspath, name) :
 		
 	# read the comment and adjust the name
 	song = eyed3.load(abspath)
-	try :
-		new_name = unicodedata.normalize('NFKD', tag(song)).encode('ascii','ignore')
-		# catch the empty names
-		if len(new_name) <= 1 :
-			raise ValueError('Unicode named song, without a known translation for it...')
-	except ValueError, e :
+	if song is None :
+		tag = lambda x : name
+
+	if not manual_help :
+		try :
+			new_name = unicodedata.normalize('NFKD', tag(song)).encode('ascii','ignore')
+			# catch the empty names
+			if len(new_name) <= 1 :
+				raise ValueError('Unicode named song, without a known translation for it...')
+		except Exception, e :
+			manual_help = True
+	
+	# manual intervention
+	if manual_help :
 		print 'Song %s needs a name.' % (repr(abspath))
 		input = raw_input("Enter the wanted name: ")
-		song.tag._comments.set(unicode(input))
-		song.tag.save(encoding="utf8")
+		try :
+			if can_convert :
+				song.tag._comments.set(unicode(input))
+				song.tag.save(encoding="utf8")
+		except IOError, error :
+			# permission denied
+			pass
 		# return None
 		new_name = input
 		
-	# trim irelevant '_' markers
+	# trim irrelevant '_' markers
 	while new_name.startswith(unicode_prefix) :
 		new_name = new_name[1:]
 		
@@ -178,6 +195,8 @@ def updateFileName(abspath, name) :
 # Extracts the songs' paths from the playlist path
 ##
 def getPlaylistSongs(playlist) :
+	global seenSongs
+	
 	songs = []
 	
 	play_path = os.path.abspath(playlist)
@@ -191,7 +210,9 @@ def getPlaylistSongs(playlist) :
 			part_path = unescape(trimmed[ : trimmed.find('"')].encode('utf-8'), {"&apos;": "'", "&quot;": '"'}).decode('utf-8')
 			abs_path  = os.path.abspath(os.path.join(play_path, part_path))
 			file_name = part_path.split(os.path.sep)[-1]
-			file_name = updateFileName(abs_path, file_name)
+			if abs_path not in seenSongs :
+				seenSongs[abs_path] = updateFileName(abs_path, file_name)
+			file_name = seenSongs[abs_path]
 			if file_name is None :
 				continue
 			songs.append((abs_path, music_folder + file_name))
@@ -283,7 +304,11 @@ def main(args) :
 			for pc, song in play_songs[playlist] :
 				if song in add_list :
 					# add the song and update the add list
-					shutil.copy(pc, song)
+					try :
+						shutil.copy(pc, song)
+					except Exception, e:
+						print 'Failed to transfer song:', song
+						print e
 					add_list.remove(song)
 	
 	# finished
