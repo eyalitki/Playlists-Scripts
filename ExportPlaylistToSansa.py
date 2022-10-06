cb0552a69226a5aacb3c44333e3ff3b778264059
Eyal Itkin <eyal.itkin@hotmail.com>
2022-10-06 23:46:58 +0300

    [Python] Migrate to Python3
    Update the script to support Python3 including relevant unicode
    edge cases. Tested to work on my playlist and on my music player.
    
    Signed-off-by: Eyal Itkin <eyal.itkin@hotmail.com>

--- ExportPlaylistToSansa.py
+++ ExportPlaylistToSansa.py
@@ -46,10 +46,11 @@ sansa_dir			= "ENTER THE DRIVE LETTER OF THE SANSA PLAYER"  # Configure this fie
 
 # wanted playlists
 playlist_base_path	= "ENTER AN ABSOLUTE PATH TO YOUR MEDIA PLAYER PLAYLIST DIRECTORY"  # Configure this field.
-																						# For Example: "C:\\Users\\user\\Music\\Playlists"
+																						# For Example: "C:\\Users\\user\\Music\\Playlists\\"
+
 playlist_names		= [] # Configure this field.
 						 # Should include a list of playlist file names (.wpl files) from the above directory.
-my_playlists 		= map(lambda x : playlist_base_path + x, playlist_names)
+my_playlists 		= list(playlist_base_path + x for x in playlist_names)
 
 #####################################################################################
 # NOTE: additional absolute paths can be added manually to the "my_playlists" list. #
@@ -77,13 +78,13 @@ seenSongs = {}
 ##
 # Exports the songs list to a newly created playlist
 ##
-def createPlaylist(playlist, songs) :
+def createPlaylist(playlist, songs):
 	playlist_name = getPlaylistName(playlist)
 	playlist = open(playlist_folder + playlist_name + '.m3u', 'w')
 	# add the header
 	# IMPORTANT: The 1st line must be a comment, because it will always be skipped by SanDisk's firmware
 	playlist.write('# A comment to be skipped by SanDisk\'s firmware\n')
-	for song in songs :
+	for song in songs:
 		playlist.write(".." + os.path.sep + music_folder.replace(sansa_dir, "") + song[1].split(os.path.sep)[-1] + '\n')
 		
 	# close the file
@@ -92,23 +93,16 @@ def createPlaylist(playlist, songs) :
 ##
 # Exports the songs list to the newly created directory
 ##
-def exportSongs(path, songs) :
-	for song in songs :
+def exportSongs(path, songs):
+	for song in songs:
 		shutil.copy(song, os.path.join(path, song.split(os.path.sep)[-1]))
 		
 ##
 # Reads the song list from the sansa player
 ##
-def getPlayerSongs() :
-	songs = []
-	
-	# list all songs in the music base path
-	for file in os.listdir(music_folder) :
-		suffix = file.split('.')[-1].lower()
-		if suffix in ['mp3', 'wmv', 'mp4', 'm4a'] :
-			songs.append(music_folder + file)
-	
-	return songs
+def getPlayerSongs():
+	return list(music_folder + file for file in os.listdir(music_folder) \
+                if file.split('.')[-1].lower() in ('mp3', 'wmv', 'mp4', 'm4a'))
 	
 ##
 # Checks if the given string contains only ascii chars
@@ -119,7 +113,7 @@ def is_ascii(s):
 ##
 # Updates the filename so it won't include unicode chars
 ##
-def updateFileName(abspath, name) :
+def updateFileName(abspath, name):
 	# should check for .mp3 
 	suffix 			= abspath.split('.')[-1]
 	can_convert		= suffix.lower() == 'mp3'
@@ -129,51 +123,51 @@ def updateFileName(abspath, name) :
 	
 	# decide what to do
 	# need to convert and can't do it :(
-	if need_convert and not can_convert :
+	if need_convert and not can_convert:
 		manual_help = True
 		
 	# can't convert, but the name is ascii
-	elif not can_convert :
+	elif not can_convert:
 		return name
 		
 	# can convert, and name is ascii
-	elif not need_convert :
-		tag = lambda x : x.tag.title
+	elif not need_convert:
+		tag = lambda x: x.tag.title
 		
 	# can convert, and needs to do it
-	else :
-		tag = lambda x : x.tag.comments.get(u"",)._text
+	else:
+		tag = lambda x: x.tag.comments.get(u"",)._text
 		
 	# read the comment and adjust the name
 	song = eyed3.load(abspath)
-	if song is None :
-		tag = lambda x : name
+	if song is None:
+		tag = lambda x: name
 
-	if not manual_help :
-		try :
-			new_name = unicodedata.normalize('NFKD', tag(song)).encode('ascii','ignore')
+	if not manual_help:
+		try:
+			new_name = unicodedata.normalize('NFKD', tag(song))
 			# catch the empty names
-			if len(new_name) <= 1 :
+			if len(new_name) <= 1:
 				raise ValueError('Unicode named song, without a known translation for it...')
-		except Exception, e :
+		except Exception as e:
 			manual_help = True
 	
 	# manual intervention
-	if manual_help :
-		print 'Song %s needs a name.' % (repr(abspath))
-		input = raw_input("Enter the wanted name: ")
-		try :
-			if can_convert :
-				song.tag._comments.set(unicode(input))
+	if manual_help:
+		print(f'Song {abspath} needs a name.')
+		user_input = input("Enter the wanted name: ")
+		try:
+			if can_convert:
+				song.tag._comments.set(user_input)
 				song.tag.save(encoding="utf8")
-		except IOError, error :
+		except IOError as error:
 			# permission denied
 			pass
 		# return None
-		new_name = input
+		new_name = user_input
 		
 	# trim irrelevant '_' markers
-	while new_name.startswith(unicode_prefix) :
+	while new_name.startswith(unicode_prefix):
 		new_name = new_name[1:]
 		
 	# add the prefix for it (if needed)
@@ -182,7 +176,7 @@ def updateFileName(abspath, name) :
 ##
 # Extracts the songs' paths from the playlist path
 ##
-def getPlaylistSongs(playlist) :
+def getPlaylistSongs(playlist):
 	global seenSongs
 	
 	songs = []
@@ -191,17 +185,17 @@ def getPlaylistSongs(playlist) :
 	play_path = os.path.sep.join(play_path.split(os.path.sep)[:-1])
 	
 	# traverse the lines
-	for line in codecs.open(playlist, encoding='utf-8').readlines() :
+	for line in codecs.open(playlist, encoding='utf-8').readlines():
 		trimmed = line.strip()
-		if trimmed.startswith(song_prefix) :
-			trimmed = trimmed[len(song_prefix) : ]
-			part_path = unescape(trimmed[ : trimmed.find('"')].encode('utf-8'), {"&apos;": "'", "&quot;": '"'}).decode('utf-8')
+		if trimmed.startswith(song_prefix):
+			trimmed = trimmed[len(song_prefix):]
+			part_path = unescape(trimmed[:trimmed.find('"')], {"&apos;": "'", "&quot;": '"'})
 			abs_path  = os.path.abspath(os.path.join(play_path, part_path))
 			file_name = part_path.split(os.path.sep)[-1]
-			if abs_path not in seenSongs :
+			if abs_path not in seenSongs:
 				seenSongs[abs_path] = updateFileName(abs_path, file_name)
 			file_name = seenSongs[abs_path]
-			if file_name is None :
+			if file_name is None:
 				continue
 			songs.append((abs_path, music_folder + file_name))
 	
@@ -211,52 +205,52 @@ def getPlaylistSongs(playlist) :
 ##
 # Extracts the playlist's name from it's full path
 ##
-def getPlaylistName(filename) :
+def getPlaylistName(filename):
 	return filename.split(os.path.sep)[-1].split('.')[0]
 
 ##
 # Prints Usage instructions
 ##
-def printUsage(args) :
-	print 'Usage: %s' % (args[0].split(os.path.sep)[-1])
-	print 'Exiting'
+def printUsage(args):
+	print('Usage: %s' % (args[0].split(os.path.sep)[-1]))
+	print('Exiting')
 	exit(1)
 
 ##
 # Main function
 ##
-def main(args) :
+def main(args):
 	# check the args
 	if len(args) != 1 + 0:
-		print 'Wrong amount of arguments, expected 0.'
+		print('Wrong amount of arguments, expected 0.')
 		printUsage(args)
 		
 	# silence the library
 	eyed3.log.setLevel("ERROR")
 	
 	# look for a dry run
-	if flag_dry_run :
-		print 'NOTICE: running in "dry run" mode, no actions will be performed on the player'
+	if flag_dry_run:
+		print('NOTICE: running in "dry run" mode, no actions will be performed on the player')
 	
 	# 1st Get a list of the songs on the player
 	song_list = getPlayerSongs()
-	print 'Read the song list from the player - %d songs' % (len(song_list))
+	print(f'Read the song list from the player - {len(song_list)} songs')
 	
 	# 2nd For each playlist get it's song list
 	play_songs	= {}
 	pc_songs 	= set([])
-	for playlist in my_playlists :
+	for playlist in my_playlists:
 		# return a list of tuple: (PC path, Player path)
 		play_songs[playlist] = getPlaylistSongs(playlist)
-		print 'Parsed the playlist %s - %d songs' % (getPlaylistName(playlist), len(play_songs[playlist]))
+		print(f'Parsed the playlist {getPlaylistName(playlist)} - {len(play_songs[playlist])} songs')
 		
 		# 3rd Write the playlist files to the player
-		if not flag_dry_run :
+		if not flag_dry_run:
 			createPlaylist(playlist, play_songs[playlist])
-			print 'Created the matching playlist on the sansa player'
+			print('Created the matching playlist on the sansa player')
 		
 		# update the pc song set
-		for pc, song in play_songs[playlist] :
+		for pc, song in play_songs[playlist]:
 			pc_songs.add(song)
 		
 	# 4th build the add_list and remove_list for the sansa player
@@ -270,12 +264,12 @@ def main(args) :
 	##########
 	
 	# dry run
-	if flag_dry_run :
-		print 'Can remove unwanted songs - %d songs' % (len(remove_list))
+	if flag_dry_run:
+		print(f'Can remove unwanted songs - {len(remove_list)} songs')
 	# actual removal (if configured)
 	elif flag_should_remove_unused_songs :
-		print 'Removing the unwanted songs - %d songs' % (len(remove_list))
-		for song in remove_list :
+		print('Removing the unwanted songs - {len(remove_list)} songs')
+		for song in remove_list:
 			os.remove(song)
 
 	#######
@@ -283,24 +277,24 @@ def main(args) :
 	#######
 	
 	# dry run
-	if flag_dry_run :
-		print 'New songs can be added - %d songs' % (len(add_list))
+	if flag_dry_run:
+		print(f'New songs can be added - {len(add_list)} songs')
 	# actual addition
-	else :
-		print 'Adding the new songs - %d songs' % (len(add_list))
-		for playlist in my_playlists : 
-			for pc, song in play_songs[playlist] :
-				if song in add_list :
+	else:
+		print(f'Adding the new songs - {len(add_list)} songs')
+		for playlist in my_playlists:
+			for pc, song in play_songs[playlist]:
+				if song in add_list:
 					# add the song and update the add list
-					try :
+					try:
 						shutil.copy(pc, song)
-					except Exception, e:
-						print 'Failed to transfer song:', song
-						print e
+					except Exception as e:
+						print('Failed to transfer song: ' + song)
+						print(e)
 					add_list.remove(song)
 	
 	# finished
-	print 'Finished successfully'
+	print('Finished successfully')
 
 # actually start
 if __name__ == "__main__":
